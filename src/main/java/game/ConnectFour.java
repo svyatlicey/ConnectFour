2971030fe1394c788f208ba574eb2f2b415e47ac
Святослав <kurenkov.ad@yandex.ru>
2026-03-07 02:29:36 +0700

    Add getCell() trivial implementation with todo

--- src/main/java/game/ConnectFour.java
+++ src/main/java/game/ConnectFour.java
@@ -2,12 +2,19 @@ package game;
 
 public class ConnectFour {
 
-    public int getRows(){
+    public int getRows() {
         //todo Реализовать логику возвращения количества строк на поле
         return 6;
     }
-    public int getColumns(){
+
+    public int getColumns() {
         //todo Реализовать логику возвращение количества столбцов на поле
         return 7;
     }
+
+    public int getCell(int row, int col){
+        //todo реализовать логику возвращения значения в клетке поля
+        return 0;
+    }
+
 }
